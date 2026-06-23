a3a6f2910fc31984a4db38ae4f2693964908714b
silverThunder09 <jeg9017@gmail.com>
2026-06-23 09:25:11 +0900

    first commit

---
+++ src/main/java/com/sparta/cabbagetest/CabbageTestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cabbagetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabbageTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabbageTestApplication.class, args);
+    }
+
+}
